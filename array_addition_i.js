46a0d319832f544435447a7451a8ae9348bf41d1
Andre Watts <andrewatts85@gmail.com>
2015-04-15 11:45:15 -0700

    beautify array_addition_i

--- array_addition_i.js
+++ array_addition_i.js
@@ -3,14 +3,14 @@
 
 //Using the JavaScript language, have the function arrayAdditionI(arr) take the array of numbers stored in arr and return the string true if any combination of numbers in the array can be added up to equal the largest number in the array, otherwise return the string false. For example: if arr contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain all the same elements, and may contain negative numbers.
 
-var x = [3,5,-1,8,12]; //true
-var y = [5,7,16,1,2]; //false
+var x = [3, 5, -1, 8, 12]; //true
+var y = [5, 7, 16, 1, 2]; //false
 
 function arrayAdditionI(arr) {
 
   // create a function that sorts numbers in descending order in an array
   function sortNums(a, b) {
-  	return b-a;
+    return b - a;
   }
 
   // sort the numbers in arr
@@ -27,9 +27,12 @@ function arrayAdditionI(arr) {
   // update the sum with the result
   // if the sum is less than the next number, skip it
   // repeat till the end of the array
-  for (var i=0; i<arr.length; i++) {
-    if (sum >= arr[i]) {sum -= Math.abs(arr[i]);}
-    else if (sum < arr[i]) {arr[i+1];}
+  for (var i = 0; i < arr.length; i++) {
+    if (sum >= arr[i]) {
+      sum -= Math.abs(arr[i]);
+    } else if (sum < arr[i]) {
+      arr[i + 1];
+    }
   }
 
   // if the sum is = to 0 return "true" else return "false"
@@ -37,4 +40,4 @@ function arrayAdditionI(arr) {
 
 }
 
-arrayAdditionI([1,2,3,100]);
+arrayAdditionI([1, 2, 3, 100]);
